2cf7454256d2a913cf9bbe5ce68e5e79cead377b
Dan Guinn <nuntius.rex@gmail.com>
2016-03-26 19:42:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?PHP
 //============================================================================================
-// Note: This block of code is used to display the file as code:
-	if(isset($_GET["code"])){
-		highlight_file( __FILE__ );	die();
-	}
-// This single page PHP is coded in a simple in-line method to quickly and simplistically 
-// convey the logic for the demo. It is not intended to imply a preference as opposed to 
-// OOP or MVC methodologies by the author.
-// 
+// Caching a JSON feed with PHP
 // Author: Dan Guinn - danguinn.com
 //============================================================================================
 
